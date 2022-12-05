0d52ecb613ed7d8097b74ad845ff2c7520792ef9
EdinsonUPC <u20201a047@upc.edu.pe>
2022-12-04 21:21:44 -0500

    Primera carga

---
+++ src/api/orden-compra-detalle/services/orden-compra-detalle.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * orden-compra-detalle service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::orden-compra-detalle.orden-compra-detalle');
